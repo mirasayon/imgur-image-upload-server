cef945ecd54d8bb1ce6418aaa12d58f9ef05d32b
mirasayon <mirasayon@ya.ru>
2024-06-26 14:30:07 +0500

    final

--- sources/app.ts
+++ sources/app.ts
@@ -1,18 +1,20 @@
-import express, { urlencoded, json } from "express";
+import Express, { urlencoded, json } from "express";
 import { uploadFile } from "./imgur.js";
-import * as nodeFS from "node:fs/promises";
-import * as node_process from "node:process";
 import fileUpload from "express-fileupload";
+import { mkdir, unlink, writeFile } from "fs/promises";
+import { join } from "node:path";
+import { existsSync } from "node:fs";
 
-const __dirname = node_process.cwd();
-const app = express();
+const __dirname = process.cwd();
+const app = Express();
+const temp_folder_path = join(__dirname, "temp");
+if (!existsSync(temp_folder_path)) {
+	await mkdir(temp_folder_path);
+}
 
 app.use(fileUpload());
 
-const PORT = Number(process.env.PORT) || 3000;
-
-app.set("view engine", "ejs");
-app.set("views", "views");
+const port = Number(process.env.PORT) || 3000;
 
 const encoded = urlencoded({ extended: false });
 
@@ -20,24 +22,21 @@ app.use(encoded);
 app.use(json());
 
 app.get("/", (_req, reply) => {
-	reply.render("index.ejs");
+	reply.send("Hello. Upload your images to the /upload path with post method");
 });
-
-app.post("/upload", async (request, response) => {
-	if (!request.files) {
-		return response.status(400).send("No files were uploaded.");
+app.post("/upload", async (req, reply) => {
+	if (!req.files) {
+		return reply.status(400).send("No files were uploaded.");
 	}
-	const sampleFile = request.files.sampleFile as fileUpload.UploadedFile;
+	const image_ = req.files.sampleFile as fileUpload.UploadedFile;
 
-	const uploadPath = `${__dirname}/temp/${sampleFile.name}`;
-	const uploadPathfs = `./temp/${sampleFile.name}`;
-	await nodeFS.writeFile(uploadPathfs, sampleFile.data);
+	const uploadPath = join(__dirname, "temp", image_.name);
+	await writeFile(uploadPath, image_.data);
 	const image = await uploadFile(uploadPath);
-	response.render("uploaded.ejs", { link: image.link });
-	await nodeFS.unlink(uploadPath);
+	reply.json({ link: image.link });
+	await unlink(uploadPath);
 });
 
-app.listen({ port: PORT }, () => {
-	console.log(`Server started on http://localhost:${PORT}/`);
+app.listen({ port: port }, () => {
+	console.log(`Server started on http://localhost:${port}/`);
 });
-
--- sources/imgur.ts
+++ sources/imgur.ts
@@ -9,36 +9,56 @@ export async function uploadFile(path: PathLike) {
 	const form = new FormData();
 	const payload = createReadStream(path);
 	if (!path) {
-		return console.error("No file to upload");
+		throw new Error("No file to upload");
 	}
 	form.append("image", payload);
 
-	const options: type_option = {
+	const options: got.OptionsInit = {
 		url: "https://api.imgur.com/3/upload",
 		method: "POST",
 		encoding: "utf8",
 		headers: { Authorization: `Client-ID ${default_client_id}` },
 		body: form,
 	};
-	const { statusCode, statusMessage, body: body_ } = await _request(options);
+	const { statusCode, statusMessage, body } = (await got.default(options)) as got.Response<string>;
 	if (statusCode !== 200) {
 		console.error(`status code: ${statusCode}`);
 		throw new Error(statusMessage);
 	}
-	return JSON.parse(body_).data;
-}
-async function _request(options: type_option): Promise<got.Response<string>> {
-	const got_ = (await got.default(options as got.OptionsInit)) as got.Response<string>;
-	return got_;
+
+	const res = JSON.parse(body).data as imgur_res;
+	console.info(JSON.parse(body).data);
+	return res;
 }
 
-type type_option = {
-	url: string;
-	method: string;
-	encoding: string;
-	headers: {
-		Authorization: string;
-	};
-	body: FormData;
+type imgur_res = {
+	id: string;
+	deletehash: string;
+	account_id: null;
+	account_url: null;
+	ad_type: null;
+	ad_url: null;
+	title: null;
+	description: null | string;
+	name: string;
+	type: string;
+	width: number;
+	height: number;
+	size: number;
+	views: number;
+	section: null;
+	vote: null;
+	bandwidth: number;
+	animated: boolean;
+	favorite: boolean;
+	in_gallery: boolean;
+	in_most_viral: boolean;
+	has_sound: boolean;
+	is_ad: boolean;
+	nsfw: null;
+	link: string;
+	tags: [];
+	datetime: number;
+	mp4: string;
+	hls: string;
 };
-
